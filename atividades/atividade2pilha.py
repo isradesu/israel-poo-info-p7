332a334f1caea0ad59aeea0d71f52e64d08fa9cf
isradesu <80715640+isradesu@users.noreply.github.com>
2021-04-17 17:01:20 -0300

    Update and rename atividade2.py to atividade2pilha.py

---
+++ atividades/atividade2pilha.py
@@ -0,0 +1,18 @@
+pilha = []
+
+pilha.append(14)
+pilha.append(10)
+pilha.append(9)
+pilha.append(7)
+pilha.append(6)
+pilha.append(8)
+
+print('Pilha: ')
+print(pilha)
+
+print('Retirando: ', pilha.pop())
+print('Retirando: ', pilha.pop())
+print('Pilha após retirada: ', pilha)
+
+print('Adicionando: ', pilha.append(55))
+print('Pilha após adição: ', pilha)
