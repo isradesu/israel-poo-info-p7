4628b5061216f5eec53554c14642f3376f7aaf1e
isradesu <80715640+isradesu@users.noreply.github.com>
2021-04-17 17:01:53 -0300

    Create atividade2fila.py

---
+++ atividades/atividade2fila.py
@@ -0,0 +1,15 @@
+fila = []
+
+fila.append(14)
+fila.append(10)
+fila.append(9)
+fila.append(7)
+fila.append(6)
+fila.append(8)
+
+print("Fila")
+print(fila)
+
+print('Retirando: ', fila.pop(0))
+print('Retirando: ', fila.pop(0))
+print('Fila após remover elementos: ', fila)
