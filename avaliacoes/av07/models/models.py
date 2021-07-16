773025d7a6d697c3e017e0478157317c8cfbca6d
Israel Leite <80715640+isradesu@users.noreply.github.com>
2021-07-16 12:33:24 -0300

    Update models.py

--- avaliacoes/av07/models/models.py
+++ avaliacoes/av07/models/models.py
@@ -1,61 +1,145 @@
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-from sqlalchemy import Table, Column, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship, backref, session, query
-from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
 
-Base = declarative_base()
+class Cliente(db.Model):
+    def __init__(self, id, nome, codigo, cnpjcpf, tipo):
+        self.id = id
+        self.nome = nome
+        self.codigo = codigo
+        self.cnpjcpf = cnpjcpf
+        self.tipo = tipo
+
+    def to_json(self):
+        return {"id": self.id, "nome": self.nome, "codigo": self.codigo, "cnpjcpf": self.cnpjcpf}
+
+    def str(self):
+        string = f"\nId={self.id} Codigo={self.codigo} Nome={self.nome} CNPJ/CPF={self.cnpjcpf} Tipo{self.tipo}"
+        return string
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'user'
+class Produto(db.Model):
+    def __init__(self, id, codigo, descricao, valorUnitario):
+        self.id = id
+        self.codigo = codigo
+        self.descricao = descricao
+        self.valorUnitario = valorUnitario
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    def to_json(self):
+        return {"id": self.id, "codigo": self.codigo, "descricao": self.descricao, "valorUnitario": self.valorUnitario}
 
-    total_balance = db.Column(db.Float)  # Total do usuário
-    flutuation_bal = db.Column(db.Float)  # Total de lucro ou prejuízo
-
-    # Um usuário tem várias criptos e transações:
-    transactions = db.relationship('Transaction')
-    criptos = db.relationship('Cripto')
-
-
-class Transaction(db.Model):
-    __tablename__ = 'transaction'
-
-    id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(150))
-    nome_cripto = db.Column(db.String(150))
-    quant = db.Column(db.Float)
-    data = db.Column(db.String(150))
-    quotation = db.Column(db.Float)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    cripto = db.relationship("Cripto", back_populates="transaction", uselist=False)
-
-    def mostrar(self):
-        tipo_maiuscula = self.tipo.upper()
-        transacao_string = f'{tipo_maiuscula} || {self.quant} {self.nome_cripto} || {self.data} || ' \
-                           f'{self.data} || {self.quotation} USD'
-        return transacao_string
+    def str(self):
+        string = f"\nId={self.id} Codigo={self.codigo} Descricao={self.descricao} Valor Unitario={self.valorUnitario}"
+        return string
 
 
-class Cripto(db.Model):
-    __tablename__ = 'cripto'
+class NotaFiscal(db.Model):
+    def __init__(self, id, codigo, cliente, lista_itens=None):
+        self.id = id
+        self.codigo = codigo
+        self.cliente = cliente
+        self.lista_itens = lista_itens
+        self.data = datetime.datetime.now()
+        self.itens = []
+        self.valorNota = 0.0
 
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(45), unique=True)
-    total_balance = db.Column(db.Float)
-    quotation = db.Column(db.Float)
-    flutuation = db.Column(db.Float)
+    def str_itens(self):
+        if self.lista_itens is None:
+            itens_json = [item for item in self.itens]
+            return itens_json
+        else:
+            return self.lista_itens
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def to_json(self):
+        return {"id": self.id, "codigo": self.codigo, "cliente": self.cliente, "itens": self.str_itens(),
+                "data": self.data_nota(), "valorNota": self.valorNota}
 
-    transaction_id = db.Column(db.Integer, db.ForeignKey("transaction.id"))
-    transaction = db.relationship("Transaction", back_populates="cripto")
+    def set_cliente(self, cliente):
+        if isinstance(cliente, Cliente):
+            self.cliente = cliente
+
+    def data_nota(self):
+        lista_data_e_hora = str(self.data).split()
+        data_lista = lista_data_e_hora[0].split('-')
+        data_final_nota = f'{data_lista[2]}/{data_lista[1]}/{data_lista[0]}'
+        return data_final_nota
+
+    def adicionar_item(self, item):
+        self.itens.append(item)
+
+    def calcular_total_nota(self, controle=False):
+        valor = 0.0
+        for item in self.itens:
+            valor = valor + item["valorItem"]
+        self.valorNota = valor
+        if controle is True:
+            return valor
+
+    def get_sequencial(self, item):
+        seq = str(item['sequencial'])
+        if len(seq) > 2:
+            return seq
+
+        elif len(seq) > 1:
+            return f'0{seq}'
+
+        return f'00{seq}'
+
+    def imprimir_nota_fiscal(self):
+
+        json_nota = {
+            'NOTA FISCAL': self.data_nota(),
+            'Cliente': self.cliente['codigo'],
+            'Nome': self.cliente['nome'],
+            'CPF/CNPJ': self.cliente['cnpjcpf'],
+            'ITENS': '',
+        }
+        lista_itens_nota = [json_nota]
+
+        if len(self.itens) > 0:
+            for item_nota in self.itens:
+                string_item = {
+                    'SEQ': self.get_sequencial(item_nota),
+                    'Descricao': item_nota['descricao'],
+                    'Quantidade': item_nota['quantidade'],
+                    'ValorUnitario': item_nota['valorUnitario'],
+                    'ValorItem': item_nota['valorItem']
+                }
+                lista_itens_nota.append(string_item)
+
+        string_final = {
+            'Valor total': self.valorNota
+        }
+        lista_itens_nota.append(string_final)
+        return lista_itens_nota
+
+
+class ItemNotaFiscal(db.Model):
+
+    def __init__(self, id, sequencial, quantidade, produto):
+        self.id = id
+        self.sequencial = sequencial
+        self.quantidade = quantidade
+        self.produto = produto
+        self.descricao = self.produto['descricao']
+        self.valorUnitario = self.produto['valorUnitario']
+        self.valorItem = float(self.quantidade * self.valorUnitario)
+
+    def to_json(self):
+        return {"id": self.id, "sequencial": self.sequencial, "quantidade": self.quantidade,
+                "produto": self.produto, "descricao": self.descricao, "valorUnitario": self.valorUnitario,
+                "valorItem": self.valorItem}
+
+    def str(self):
+        string = f"\nId={self.id} Sequencial={self.sequencial} Quantidade={self.quantidade} Produto={self.descricao} Valor Unitario={self.valorUnitario} Valor Item={self.valorItem}"
+        return string
+
+    def get_sequencial(self):
+        seq = str(self.sequencial)
+        if len(seq) > 2:
+            return seq
+
+        elif len(seq) > 1:
+            return f'0{seq}'
+
+        return f'00{seq}'
+
